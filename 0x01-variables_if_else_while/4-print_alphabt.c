3a7e1efd320ee40138d1962251d643781b27e0ef
Bernard Worthy <worthyizuchukwu@gmail.com>
2023-08-11 16:14:29 -0700

    Taks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'e')
+		{
+			continue;
+		}
+		else if (alph == 'q')
+		{
+			continue;
+		}
+		else
+			putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
