a698a57501ceda680eb2effae26c29f8b8e309f3
Bernard Worthy <worthyizuchukwu@gmail.com>
2023-08-11 17:26:53 -0700

    tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
