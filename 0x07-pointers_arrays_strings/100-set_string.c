7fbba649787817e035bb94b0274b9a1566433e9c
Bernard Worthy <worthyizuchukwu@gmail.com>
2023-08-29 16:57:42 -0700

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to char.
+ * @s: source address.
+ * @to: target address.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
